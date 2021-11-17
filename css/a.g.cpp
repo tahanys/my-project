b1d2b795a86f8d27504ec8336c243fd6ecbc0adf
TahanyS <tahanysalah1987@yahoo.com>
2021-11-17 06:55:18 +0200

    create my project

---
+++ css/a.g.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for ( int i=1; i<=5;i++)
+
+    {
+    	for (int j=1;j<i;i++)
+	 cout << " "<<endl;
+    	
+    	
+
+	 
+	 
+	 for ( int s=5; s> i; s--)	
+
+	 	
+	 cout <<"*";
+	  cout << " "<<endl;
+			
+	 	
+	 	
+	 }
+	  
+		
+		
+		
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
