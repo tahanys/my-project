deeef4d16c3b96533b452dfe0826b56a2eecd9af
TahanyS <tahanysalah1987@yahoo.com>
2021-11-17 07:40:02 +0200

    make modified and new file

---
+++ git/p.p.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+ { int a;
+ 
+ for (int i =1; i <=5;i++ )
+ {
+ 	for (int i =1; i <=5;i++ )
+ 	cout << "*";
+ 	cout << " "<< endl;
+ }
+  
+ 	
+ 	
+ 	
+ 
+ 
+
+ 		
+ 		
+	
+	                                        
+ 		
+ 		
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
